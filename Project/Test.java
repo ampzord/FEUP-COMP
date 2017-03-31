5fd3e267551d90740ba633b410d5932edcab592b
António Pereira <antonio_m_sp@hotmail.com>
2017-03-31 12:19:49 +0100

    a

---
+++ Project/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+// A comment
+public static int test() {
+	return 0;
+}
+	
+
+    public static void main(String[] args) {
+    	if (x == 2) {
+    		
+    	}
+    }
+    
+}
