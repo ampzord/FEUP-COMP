0f52754670ae6277b38ec2cbf26c4cae16ae35de
Nuno <nuno.freitas96@gmail.com>
2017-06-02 02:54:02 +0100

    Testing of javaparser library, obtained strategy

--- Project/Test.java
+++ Project/Test.java
@@ -1,15 +1,18 @@
-public class Test {
+public class Pattern {
+	void p1() {		
+		if (_at_x == 2) {
+			_at_x = a;
+		}
+	}
+	void p2() {		
+		if (_at_y) {
+			_at_a = _at_a + x;
+			System.out.println("aiai");
+		}
+	}
+	void p3() {		
+		if (_at_z == _at_h + 2) {
+			_at_a = i;
 
-// A comment
-public static int test() {
-	return 0;
-}
-	
-
-    public static void main(String[] args) {
-    	if (x == 2) {
-    		
-    	}
-    }
-    
-}
+	}
+}}
