4525865b88ebcae90bb20a5914d0ffdf27fe49be
AMPZORD <remember>
2017-06-05 20:59:03 +0100

    Revert "Changed Project folder."
    This reverts commit 63112b8527f306348bfb31845e4048611623b265.

---
+++ Project/Test.java
@@ -0,0 +1,13 @@
+public class Pattern {
+	void p1(boolean _at_x) {
+		
+
+		
+	
+			_at_a = _at_x + _at_b;
+
+		
+		
+		
+	}
+}
---
+++ Project/Test2.java
@@ -0,0 +1,11 @@
+public class Stuff {
+	void p1(int x) {
+		
+		
+			x = b + b;
+			x = a + b;
+
+		
+		
+	}
+}
